dc6836704ad686e2cb8a81696ad8143ba54922d1
jaio_arte <jaione.arnaez@gmail.com>
2025-02-28 09:20:06 +0100

    ability selector

--- src/app/containers/home.component.ts
+++ src/app/containers/home.component.ts
@@ -1,16 +1,36 @@
-import { JsonPipe } from '@angular/common';
+import { JsonPipe, TitleCasePipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, input } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
 import { PokeImgComponent } from '../components/poke-img.component';
-import { PokeResponse } from '../models/poke-api';
+import { Abilities, PokeResponse, RootAbilities } from '../models/poke-api';
 import { PokemonImagePipe } from '../pipes/pokemon-image.pipe';
 
 @Component({
   selector: 'app-home',
-  imports: [PokemonImagePipe, JsonPipe, PokeImgComponent],
+  imports: [PokemonImagePipe, JsonPipe, PokeImgComponent, TitleCasePipe],
   template: `
+    <div class="controls-container">
+      <label
+        >Selecciona la habilidad:
+        <select (change)="onSelectAbility($event)">
+          @if (abilities.value()) {
+            <option [value]="''">None</option>
+
+            @for (ability of abilities.value()?.results; track ability) {
+              <option [value]="ability.url">
+                {{ ability.name | titlecase }}
+              </option>
+            }
+          }
+          @if (abilities.isLoading()) {
+            <option class="loading-text">Loading...</option>
+          }
+        </select>
+      </label>
+    </div>
+
     @if (pokedex.isLoading()) {
       <p>LOADING...</p>
     } @else if (pokedex.error()) {
@@ -31,16 +51,32 @@ import { PokemonImagePipe } from '../pipes/pokemon-image.pipe';
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  // from Angular Router's withComponentInputBinding
   queryLimit = input<number>();
+  defaultLimit: Readonly<number> = 6;
+
+  selectedAbility = signal<string | null>(null);
 
   private http = inject(HttpClient);
 
   pokedex = rxResource({
     request: () => ({
+      selectedAbility: this.selectedAbility(),
       limit: this.queryLimit(),
     }),
-    loader: ({request}) => {
+    loader: ({ request }) => {
       const limit = request.limit ?? 1000000;
+      if (request.selectedAbility) {
+        return this.http.get<RootAbilities>(`${request.selectedAbility}`).pipe(
+          // tap((_) => console.log('selected', _)),
+          // reduce
+          map((resp) => {
+            return resp.pokemon
+              .map(({ pokemon }) => pokemon)
+              .filter((_, index) => index < limit);
+          }),
+        );
+      }
       return this.http
         .get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}`)
         .pipe(
@@ -51,4 +87,18 @@ export class HomeComponent {
         );
     },
   });
+
+  abilities = rxResource({
+    loader: () => {
+      return this.http.get<Abilities>(
+        `https://pokeapi.co/api/v2/ability/?limit=100000000`,
+      );
+      // .pipe(tap((_) => console.log(_)));
+    },
+  });
+
+  onSelectAbility(event: Event) {
+    const target = event.target as HTMLSelectElement; // 💕typescript
+    this.selectedAbility.set(target.value);
+  }
 }
